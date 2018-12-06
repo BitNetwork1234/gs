74beead0d4016c1f9c707cead7b1d2e4b6e86e1c
LawrenceTech <lawrenceoffline@gmail.com>
2018-12-06 10:19:46 +0000

    added

---
+++ gs.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: /login/login.php");
+  exir;
+}
+?>
+<html>
+<body>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-1814189887025045",
+          enable_page_level_ads: true
+     });
+</script>
+</body>
+</html>
