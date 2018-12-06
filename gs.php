d90c271d74dfa2d988d09a131f66674393d79178
LawrenceTech <lawrenceoffline@gmail.com>
2018-12-06 11:06:46 +0000

    Update gs.php

--- gs.php
+++ gs.php
@@ -6,18 +6,20 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 }
 ?>
 <html>
-<body>
-<object width="700" height="500" data="/g/BTD5.swf"></object>
-<object width="700" height="500" data="/g/BoxHead2.swf"></object>
-<object width="300" height="200" data="/swf/HappyWheelsUnblocked.swf"></object>
-<object width="300" height="200" data="/g/ImpossibleQuiz.swf"></object>
-
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<head>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({
           google_ad_client: "ca-pub-1814189887025045",
           enable_page_level_ads: true
      });
 </script>
+</head>
+<body>
+<object width="700" height="500" data="/g/BTD5.swf"></object>
+<object width="700" height="500" data="/g/BoxHead2.swf"></object>
+<object width="300" height="200" data="/swf/HappyWheelsUnblocked.swf"></object>
+<object width="300" height="200" data="/g/ImpossibleQuiz.swf"></object>
+
 </body>
 </html>
